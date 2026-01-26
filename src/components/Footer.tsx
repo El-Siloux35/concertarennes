e00e3cc57d3a23775e97860ce27d01e67d958a10
Cecile <hugon.cecile@gmail.com>
2026-01-26 11:26:25 +0100

    Make footer fixed at bottom of screen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-6 text-center text-primary/60 text-sm">
+    <footer className="fixed bottom-0 left-0 right-0 py-4 text-center text-primary/60 text-sm bg-background z-40">
       <span>♥︎ Captab @2026</span>
     </footer>
   );
